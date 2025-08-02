8b47c7d15afa0aefe245d811de93cbf292503dc7
vanshgavhane18 <vanshgavhane018@gmail.com>
2025-08-02 14:00:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function subscribeNow() {
+  window.open("https://www.youtube.com/channel/UCYPyUAkqiJ_VgF5pxMq4tnA", "_blank");
+  alert("🔥 Thanks for subscribing! Welcome to the savage squad!");
+}
